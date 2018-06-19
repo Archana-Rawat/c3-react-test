fd20fc255b08de2133827e836cad4d9716becf72
Archana-Rawat <archanarawat138@gmail.com>
2018-06-19 18:47:02 +0530

    test case added-Issue found->node_modules/react-scripts/scripts/test.js:20

--- src/PearsonUsers.spec.js
+++ src/PearsonUsers.spec.js
@@ -1,16 +1,47 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { PearsonUsers } from "./PearsonUsers";
+import renderer from 'react-test-renderer';
+
 
 describe("PearsonUsers", () => {
   let component;
-
   beforeEach(() => {
     component = shallow(<PearsonUsers />);
   });
 
-  it("renders a h1", () => {
-    const h1 = component.find("h1");
-    expect(h1.text()).toEqual("Pearson User Management");
+it('renders existing users', () => {
+  const users={users: [{
+    id: 4,
+    first_name: "Eve",
+    last_name: "Holt",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
+   }, {
+    id: 5,
+    first_name: "Charles",
+    last_name: "Morris",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"
+   }, {
+    id: 6,
+    first_name: "Tracey",
+    last_name: "Ramos",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
+   }]};
+
+  const tree = renderer.create(<PearsonUsers users={ users }/>);
+  expect(tree).toMatchSnapshot();
+});
+
+ it('It shows the rendered data', () => {
+    component = shallow(<PearsonUsers />);
+    expect(component.find('.mainDiv').length).toEqual(1);
   });
+
+
+ it('Delete functionality', () => {
+   component.instance().handleDelete() 
+ const tree = component.instance().handleDelete();
+  expect(tree).toMatchSnapshot();
+  })
+
 });
